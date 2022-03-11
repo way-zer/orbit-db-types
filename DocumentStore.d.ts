39e14748a848004918fd1dada8c9b273e26e6a3a
way-zer <himc.wicp@gmail.com>
2022-03-12 01:03:35 +0800

    (main) 修复数据库的部分问题

--- DocumentStore.d.ts
+++ DocumentStore.d.ts
@@ -2,7 +2,7 @@ declare module "orbit-db-docstore" {
     import Store from "orbit-db-store";
 
     export default class DocumentStore<T extends { _id: string }> extends Store {
-        get(key: string, caseSensitive?: boolean): T
+        get(key: string, caseSensitive?: boolean): T[]
         query(filter: (doc: T) => boolean, options?: { fullOp: boolean }): T[]
 
         batchPut(docs: T[]): Promise<void>
